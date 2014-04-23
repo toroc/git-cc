ca3f7a1ac86ddd4a4fd7cbcc84874fc2dd845219
Phil Tsarik <phil.tsarik@gmail.com>
2014-04-23 13:45:13 +0300

    New gitcc rerebase command
    Created as a shortcut of reset+tag+rebase.

---
+++ rerebase.py
@@ -0,0 +1,11 @@
+"""Rebase from Clearcase starting from master_cc.
+   Ususally used after gitcc checkin to avoid duplication of commits: with real data and with changes in ".gitcc".
+"""
+
+from common import *
+import rebase
+
+def main():
+    reset()
+    tag(CI_TAG)
+    rebase.main()
